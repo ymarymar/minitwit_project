aa23b2b77cc2004dd9de1e3d3830adb12db0524a
Ymir Arnarson <ymar@itu.dk>
2026-04-06 19:13:21 +0200

    removed latestValue and updated getLatest and setLatest to call database instead of application

--- minitwit-java/src/main/java/zerodowntime/controller/simulator/SimulatorController.java
+++ minitwit-java/src/main/java/zerodowntime/controller/simulator/SimulatorController.java
@@ -1,7 +1,7 @@
 package zerodowntime.controller.simulator;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
+
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +14,7 @@ import io.javalin.openapi.OpenApiParam;
 import io.javalin.openapi.OpenApiRequestBody;
 import io.javalin.openapi.OpenApiResponse;
 import io.javalin.openapi.OpenApiSecurity;
+import zerodowntime.DatabaseManager;
 import zerodowntime.dto.simulator.*;
 import zerodowntime.service.AuthService;
 import zerodowntime.service.MessageService;
@@ -26,7 +27,7 @@ public class SimulatorController {
     private MessageService messageService;
 
     private static final Logger log = LoggerFactory.getLogger(SimulatorController.class);
-    private static final AtomicInteger latestValue = new AtomicInteger(0);
+    
 
     public SimulatorController(AuthService authService, UserService userService, MessageService messageService) {
         this.authService = authService;
@@ -46,10 +47,9 @@ public class SimulatorController {
     )
     public void getLatest(Context ctx) {
         try {
-            ctx.json(new LatestValue(latestValue.get()));
+            ctx.json(new LatestValue(DatabaseManager.getLatest()));
         } catch (Exception e) {
             log.error("[getLatest] Error: {}", e.getMessage());
-            e.printStackTrace();
             ctx.status(500).json(new ErrorResponse(500, "Internal server error"));
         }
     }
@@ -283,7 +283,7 @@ public class SimulatorController {
         String latestParam = ctx.queryParam("latest");
         if (latestParam != null) {
             try {
-                latestValue.set(Integer.parseInt(latestParam));
+                DatabaseManager.setLatest(Integer.parseInt(latestParam));
             } catch (NumberFormatException ex) {
                 log.error("[updateLatest] Error: {}", ex.getMessage(), ex);            
             }
