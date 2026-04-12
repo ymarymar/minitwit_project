8165aba8c79065735f9e8f0e8795369f69d52c87
Corbijn Bulsink <jbul@itu.dk>
2026-04-12 08:55:48 +0200

    add wait 10s

--- infra/provision.sh
+++ infra/provision.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 
-cd infra/terraform
+cd terraform
 tofu apply -auto-approve
+
+echo "Waiting for droplets to boot..."
+sleep 10
 
 cd ../ansible
 ansible-playbook -i inventory.digitalocean.yml site.yml
