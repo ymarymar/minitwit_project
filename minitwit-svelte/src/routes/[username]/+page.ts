7cb77e04bd4a346319e5b0092837a5a3777ea934
Corbijn Bulsink <jbul@itu.dk>
2026-04-17 11:07:45 +0200

    add user not found error

--- minitwit-svelte/src/routes/[username]/+page.ts
+++ minitwit-svelte/src/routes/[username]/+page.ts
@@ -1,12 +1,21 @@
 import type { Message } from '$lib/types';
 import type { PageLoad } from './$types';
+import { error } from '@sveltejs/kit';
 
 export const load: PageLoad = async ({ fetch, params, url }) => {
     const page = Number(url.searchParams.get('page') ?? 0);
 
-    const result = await fetch(`/web/user/${params.username}?page=${page}`)
-        .then(r => r.ok ? r.json() : { messages: [], following: false, total: 0})
-        .catch(() => ({ messages: [], following: false, total: 0}));
+    const response = await fetch(`/web/user/${params.username}?page=${page}`);
+
+    if (response.status === 404) {
+        throw error(404, `User "${params.username}" not found`);
+    }
+
+    if (!response.ok) {
+        throw error(response.status, 'Something went wrong');
+    }
+
+    const result = await response.json();
 
     return {
         messages: result.messages as Message[],
