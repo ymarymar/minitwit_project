e179a4290da387b69a43c02372b1dd9256ca11c4
Ymir Arnarson <ymar@itu.dk>
2026-06-02 15:31:56 +0200

    added all

--- infra/provision.sh
+++ infra/provision.sh
@@ -6,6 +6,6 @@ tofu apply -auto-approve
 
 echo "Waiting for droplets to boot..."
 cd ../ansible
-ansible -i inventory.digitalocean.yml -m wait_for_connection --timeout=120
+ansible -i inventory.digitalocean.yml all -m wait_for_connection --timeout=120
 
 ansible-playbook -i inventory.digitalocean.yml site.yml
